3e56ab508902b2453a1c353e0994219a0bcde1d6
PMJ000 <qkralswp789@naver.com>
2024-12-21 15:43:00 +0900

    test

--- pmj/cli.cpp
+++ pmj/cli.cpp
@@ -5,52 +5,65 @@
 #include <arpa/inet.h>
 #include <sys/socket.h>
 using namespace std;
+class client
+{
+	private:
+		int sock;
+		char msg[1024];
+		int result[4];
+		struct sockaddr_in serv_addr;
 
-void error(const char *message);
-
+	public:
+		void create_socket(int * argc,char *argv[]) //소켓 생성 함수
+		{
+			if(*argc!=3)
+			{
+				printf("Usage : %s <IP> <port>\n", argv[0]);
+				exit(1);
+			}
+			sock=socket(PF_INET, SOCK_STREAM, 0);
+			if(sock == -1)
+				error("socket() error");
+			
+			memset(&serv_addr, 0, sizeof(serv_addr));
+			serv_addr.sin_family=AF_INET;
+			serv_addr.sin_addr.s_addr=inet_addr(argv[1]);
+			serv_addr.sin_port=htons(atoi(argv[2]));
+				
+			if(connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr))==-1) 
+				error("connect() error!");
+			else
+				puts("Connected...........");
+		}
+		void error(const char *message) // 소켓 생성중 에러문구 출력 함수
+		{
+			fputs(message, stderr);
+			fputc('\n', stderr);
+			exit(1);
+		}
+		void input() // 서버로 문자 전송 함수
+		{
+			while(1)
+			{
+				fputs("Your number : ", stdout);
+				cin>>msg;
+				if(msg[0] == 'q')
+					break;
+				write(sock,msg,1024);
+				read(sock,msg,1024);
+				cout<<msg<<endl;
+			}
+		}
+		void client_close() //소켓 닫는 함수
+		{
+			close(sock);
+		}
+};
 int main(int argc, char* argv[])
 {
-	int sock;
-    char msg[1024];
-    int result[4];
-	struct sockaddr_in serv_addr;
-	
-	if(argc!=3)
-    {
-		printf("Usage : %s <IP> <port>\n", argv[0]);
-		exit(1);
-	}
-	
-	sock=socket(PF_INET, SOCK_STREAM, 0);
-	if(sock == -1)
-		error("socket() error");
-	
-	memset(&serv_addr, 0, sizeof(serv_addr));
-	serv_addr.sin_family=AF_INET;
-	serv_addr.sin_addr.s_addr=inet_addr(argv[1]);
-	serv_addr.sin_port=htons(atoi(argv[2]));
-		
-	if(connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr))==-1) 
-		error("connect() error!");
-    else
-		puts("Connected...........");
-    while(1)
-	{
-		fputs("Your number : ", stdout);
-		cin>>msg;
-		if(msg[0] == 'q')
-			break;
-		write(sock,msg,1024);
-		read(sock,msg,1024);
-		cout<<msg<<endl;
-	}
-	close(sock);
+	client pmj;
+	pmj.create_socket(&argc,argv);
+	pmj.input();
+	pmj.client_close();
 	return 0;
-}
-
-void error(const char *message)
-{
-	fputs(message, stderr);
-	fputc('\n', stderr);
-	exit(1);
 }
--- pmj/ser.cpp
+++ pmj/ser.cpp
@@ -5,62 +5,77 @@
 #include <arpa/inet.h>
 #include <sys/socket.h>
 using namespace std;
+class server
+{
+    private:
+        int serv_sock;
+        int clnt_sock;
+        struct sockaddr_in serv_adr;
+        struct sockaddr_in clnt_addr;
+        socklen_t clnt_addr_size;
+        char message[1024];
 
-void error(const char * errormessage);
+    public:
+        void input()
+        {
+            while(1)
+            {
+                int str_len=read(clnt_sock,message, 1024);
+                if(str_len==-1)
+                    error("read() error!");
+                cout<<message<<endl;
+                write(clnt_sock,message, 1024);
+            }
+        }
+        void create_socket(int * argc,char * argv[])
+        {
+            if(*argc != 2)
+            {
+                cout<<"Usage : "<<argv[0]<<"<port>"<<endl;
+                exit(1);
+            }
 
+            serv_sock = socket(PF_INET,SOCK_STREAM,0);
+            if(serv_sock == -1)
+            {
+                error("socket() error");
+            }
+
+            memset(&serv_adr, 0, sizeof(serv_adr));
+            serv_adr.sin_family=AF_INET;
+            serv_adr.sin_addr.s_addr=htonl(INADDR_ANY);
+            serv_adr.sin_port=htons(atoi(argv[1]));
+
+            if(bind(serv_sock, (struct sockaddr*)&serv_adr, sizeof(serv_adr))==-1)
+                error("bind() error");
+            if(listen(serv_sock, 2)==-1)
+                error("listen() error");
+
+            clnt_addr_size=sizeof(clnt_addr);  
+            clnt_sock=accept(serv_sock, (struct sockaddr*)&clnt_addr,&clnt_addr_size);
+            if(clnt_sock==-1)
+            {
+                error("accept() error");  
+            }
+
+        }
+        void error(const char * errormessage)
+        {
+            fputs(errormessage, stderr);
+            fputc('\n', stderr);
+            exit(1);
+        }
+        void socket_close()
+        {
+            close(clnt_sock);	
+	        close(serv_sock);
+        }
+};
 int main(int argc, char *argv[])
 {
-    int serv_sock;
-    int clnt_sock;
-
-    struct sockaddr_in serv_adr;
-    struct sockaddr_in clnt_addr;
-	socklen_t clnt_addr_size;
-    char message[1024];
-    if(argc != 2)
-    {
-        cout<<"Usage : "<<argv[0]<<"<port>"<<endl;
-        exit(1);
-    }
-
-    serv_sock = socket(PF_INET,SOCK_STREAM,0);
-    if(serv_sock == -1)
-    {
-        error("socket() error");
-    }
-
-    memset(&serv_adr, 0, sizeof(serv_adr));
-    serv_adr.sin_family=AF_INET;
-	serv_adr.sin_addr.s_addr=htonl(INADDR_ANY);
-	serv_adr.sin_port=htons(atoi(argv[1]));
-
-    if(bind(serv_sock, (struct sockaddr*)&serv_adr, sizeof(serv_adr))==-1)
-		error("bind() error");
-	if(listen(serv_sock, 2)==-1)
-		error("listen() error");
-
-    clnt_addr_size=sizeof(clnt_addr);  
-	clnt_sock=accept(serv_sock, (struct sockaddr*)&clnt_addr,&clnt_addr_size);
-	if(clnt_sock==-1)
-    {
-		error("accept() error");  
-    }
-    while(1)
-    {
-        int str_len=read(clnt_sock,message, 1024);
-        if(str_len==-1)
-            error("read() error!");
-        cout<<message<<endl;
-        write(clnt_sock,message, 1024);
-    }
-	close(clnt_sock);	
-	close(serv_sock);
+    server pmj;
+    pmj.create_socket(&argc,argv);
+    pmj.input();
+    pmj.socket_close();
     return 0;
-}
-
-void error(const char * errormessage)
-{
-	fputs(errormessage, stderr);
-	fputc('\n', stderr);
-	exit(1);
 }
