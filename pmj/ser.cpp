d137a683be5d95a0b348b70b778f7fc0a905c839
PMJ000 <qkralswp789@naver.com>
2024-12-23 15:41:35 +0900

    ㅅ

--- pmj/ser.cpp
+++ pmj/ser.cpp
@@ -4,7 +4,46 @@
 #include <unistd.h>
 #include <arpa/inet.h>
 #include <sys/socket.h>
+#include <mariadb/conncpp.hpp>
 using namespace std;
+// class database
+// {
+//     private:
+
+//     public:
+//          // DB연결 객체 생성
+//         sql::Driver* driver = sql::mariadb::get_driver_instance();
+//         // 연결할 DB의 특정 IP, DB를 정의
+//         sql::SQLString url("jdbc:mariadb://localhost:3306/KINGSEO");
+//         // 연결할 DB를 사용할 유저를 정의
+//         sql::Properties properties({{"user", "PMJ"}, {"password", "1234"}});
+//         // 객체에 값을 통하여 연결을 시도
+//         std::unique_ptr<sql::Connection> conn(driver->connect(url, properties));
+//             void select(std::unique_ptr<sql::Connection> &conn) 
+//         {
+//             try {
+//                 // createStaemet 객체 생성
+//                 std::unique_ptr<sql::Statement> stmnt(conn->createStatement());
+//                 // 쿼리를 실행
+//                 sql::ResultSet *res = stmnt->executeQuery("select * from KING");
+//                 // 반복문을 통해서 내부의 값을 반환
+//                 while (res->next()) {
+//                     std::cout << "NO = " << res->getString(1);
+//                     std::cout << ",library = " << res->getString(2);
+//                     std::cout << ",dataroom = "<< res->getString(3);
+//                     std::cout << ",SNB = "<< res->getString(4);
+//                     std::cout << ",name = "<< res->getString(5);
+//                     std::cout << ",author = "<< res->getString(6);
+//                     std::cout << ",publisher = "<< res->getString(7);
+//                     std::cout << ",find = "<< res->getString(8);
+//                     std::cout << ",year2 = "<< res->getString(9);
+//                 }
+//             // 실패시 오류 메세지 반환
+//             } catch(sql::SQLException& e){
+//                 std::cerr << "Error selecting tasks: " << e.what() << std::endl;
+//             }
+//         }
+// };
 class server
 {
     private:
@@ -17,7 +56,7 @@ class server
         socklen_t clnt_addr_size;
         char message[1024];
         char number;
-        string book[10] = {" 도서"," 책"," 출판사"," 대여정보"," 기타"," 제목"," 작가"," 장르"," 기타"," 등"};
+        string book[10] = {"연변","소장도서관"," 자료실명"," 등록번호","도서명","저자","출판사"," 출판연도"," 청구기호","데이터기준일자"};
 
     public:
         void login()
@@ -60,7 +99,7 @@ class server
                 {
                     cout<<"미완성";
                 }
-                else if(number == 0)
+                else if(number == '0')
                 {
                     sleep(1);
                 }
@@ -117,9 +156,9 @@ class server
 int main(int argc, char *argv[])
 {
     server pmj;
+    // database dbpmj;
     pmj.create_socket(&argc,argv);
     pmj.login();
-    cout<<"11";
     pmj.socket_close();
     return 0;
 }
